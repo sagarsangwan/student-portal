b8ba979fc215e23fdb3b418bc23148b81129db89
sagarsangwan <iamsagarsangwan@gmail.com>
2021-12-08 23:31:46 +0530

    templates related minor changes

--- server.py
+++ server.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def home():
     return render_template("pages/home.html")
 
+@app.route("/courses")
+def cse():
+    return render_template("pages/courses.html")
+
+@app.route("/feedback")
+def feedback():
+    return render_template("pages/feedback.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
